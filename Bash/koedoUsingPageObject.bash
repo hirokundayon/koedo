9ea2af5d32af472e05c3409f86e9a6453fccc551
hirokundayon <hirokundayon@users.noreply.github.com>
2016-05-27 07:19:27 +0900

    Microsoft Edgeにも対応

--- Bash/koedoUsingPageObject.bash
+++ Bash/koedoUsingPageObject.bash
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 SCRIPT_DIR=$(cd $(dirname $0);pwd)
 . ${SCRIPT_DIR}/PageObject.bash
 
-BROWSER="firefox"
+BROWSER=$1
+#BROWSER="firefox"
 #BROWSER="chrome"
 #BROWSER="ie"
 #BROWSER="Android"
@@ -19,19 +20,38 @@ URL='http://www.google.co.jp/'
 
 goURL ${SESSION_ID} ${URL}
 
+waitByTitle ${SESSION_ID} "Google"
+
 #"小江戸らぐ"と打って検索
+
 SELECTOR='name'
 LOCATOR='q'
 
 ELEMENT_ID=$(findElementByName ${SESSION_ID} q)
 
-sendKeysToElement ${SESSION_ID} ${ELEMENT_ID} '小江戸らぐ\n'
+if [ "${BROWSER}" = edge ]
+then
+#  echo ${BROWSER} 1>&2
+  curl --request "POST" \
+       --data '{"script":"elements = document.getElementsByName(\"q\"); elements[0].value=\"小江戸らぐ\";", "args":[]}' \
+			  "http://localhost:4444/wd/hub/session/"${SESSION_ID}"/execute/sync"
+  curl --request "POST" \
+       --data '{"script":"elements = document.getElementsByName(\"f\"); elements[0].submit();", "args":[]}' \
+			  "http://localhost:4444/wd/hub/session/"${SESSION_ID}"/execute/sync"
+else
+#  echo ${BROWSER} 1>&2
+  sendKeysToElement ${SESSION_ID} ${ELEMENT_ID} '小江戸らぐ\n'
+fi
 
 #日経Linuxの記事へ
-TITLE_VALUE="小江戸らぐ - Google 検索"
+TITLE_VALUE='小江戸らぐ - Google 検索'
 waitByTitle "${SESSION_ID}" "${TITLE_VALUE}"
 
-LOCATOR="OSS支える！"
+if [ "${BROWSER}" = edge ]
+then
+  sleep 5s
+fi
+LOCATOR="OSS支える"
 ELEMENT_ID=$(findElementByPartialLinkText ${SESSION_ID} "${LOCATOR}")
 
 #LOCATOR='a[href=http://itpro.nikkeibp.co.jp/article/COLUMN/20131003/508665/]'
@@ -56,6 +76,10 @@ TITLE=$(waitByTitle2 ${SESSION_ID} "${TITLE_VALUE1}" "${TITLE_VALUE2}")
 if [ "${TITLE}" = "${TITLE_VALUE1}" ] 
 then
   #広告のページが表示された時はスキップする
+  if [ "${BROWSER}" = edge ]
+  then
+    sleep 5s
+  fi
   LOCATOR="このページをスキップする"
   ELEMENT_ID=$(findElementByLinkText ${SESSION_ID} "${LOCATOR}")
 
@@ -65,8 +89,15 @@ then
 fi
 
 #写真をクリック
-LOCATOR="img[alt=小江戸らぐ]"
-ELEMENT_ID=$(findElementByCSSselector ${SESSION_ID} "${LOCATOR}")
+if [ "${BROWSER}" = edge ]
+then
+  sleep 5s
+  RESPONSE=$(curl --request "POST" --data '{"using":"css selector", "value":"a[href=\"http://itpro.nikkeibp.co.jp/article/COLUMN/20131003/508665/\?SS\=imgview\&FD=1491996558\&ST\=oss\"]"}' "http://localhost:4444/wd/hub/session/"${SESSION_ID}"/element")
+  ELEMENT_ID=$(echo ${RESPONSE} | sed -e 's/^.*"ELEMENT":"\([^"]*\)".*$/\1/g')
+else
+  LOCATOR="img[alt=小江戸らぐ]"
+  ELEMENT_ID=$(findElementByCSSselector ${SESSION_ID} "${LOCATOR}")
+fi
 
 clickElement ${SESSION_ID} ${ELEMENT_ID}
 
