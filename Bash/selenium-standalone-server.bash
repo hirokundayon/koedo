b66bc7973f5b1c33b43734f8a214125bf9d955ef
hirokundayon <hirokundayon@users.noreply.github.com>
2016-05-27 07:25:41 +0900

    Microsoft Edgeにも対応

--- Bash/selenium-standalone-server.bash
+++ Bash/selenium-standalone-server.bash
@@ -3,4 +3,6 @@
 DISPLAY=:0.0 \
  java -jar /path/to/selenium-server-standalone-2.53.0.jar \
   -Dwebdriver.chrome.driver=/path/to/chromedriver #\
-#  -Dwebdriver.ie.driver=/path/to/IEDriverServer.exe
+#  -Dwebdriver.ie.driver=/path/to/IEDriverServer.exe \
+#  -Dwebdriver.edge.driver='/path/to/MicrosoftWebDriver.exe'
+
